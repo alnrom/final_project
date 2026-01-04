c5336e1cc114a3f70f1cff0b030840a66082e5be
alnrom <aln_rom@hotmail.com>
2026-01-04 13:07:34 -0600

    bug on MenuItem title property

--- inventory/models.py
+++ inventory/models.py
@@ -19,7 +19,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 class RecipeRequirement(models.Model):
     # Links the MenuItem to its Ingredients
